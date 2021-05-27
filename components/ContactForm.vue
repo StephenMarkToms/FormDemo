e0279bc63d8c0ae87f450cc36a1554e612d027c8
Stephen Toms <s.mark.toms@gmail.com>
2021-05-27 00:18:11 -0400

    adding in rough UI elements

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -1,3 +1,21 @@
 <template>
-  <div>hello world</div>
+  <div>
+    <FormulateInput type="text" label="FIRST NAME*" required />
+    <FormulateInput type="text" label="LAST NAME*" required />
+    <FormulateInput type="email" label="EMAIL ADDRESS*" required />
+    <FormulateInput type="text" label="ORGANIZATION" required />
+    <FormulateInput
+      type="select"
+      label="EU RESIDENT*"
+      placeholder="SELECT ONE"
+      :options="[
+        { label: 'Yes', value: 'Yes' },
+        { label: 'No', value: 'No' },
+      ]"
+      required
+    />
+    <FormulateInput type="checkbox" label="ADVANCES" />
+    <FormulateInput type="checkbox" label="ALERTS" />
+    <FormulateInput type="checkbox" label="OTHER COMMUNICATIONS" />
+  </div>
 </template>
