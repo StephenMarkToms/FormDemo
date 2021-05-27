606300aa88acc1afc201408ce8f3198abaa02af4
Stephen Toms <s.mark.toms@gmail.com>
2021-05-27 00:23:57 -0400

    starting to correct layout of elements

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -1,10 +1,15 @@
 <template>
-  <div>
+  <div class="grid grid-cols-2 gap-4 max-w-2xl">
+    <div class="w-full col-span-2">
+      <div class="font-bold text-2xl">Sign up for email updates</div>
+      <div>*Indicates Required Field</div>
+    </div>
     <FormulateInput type="text" label="FIRST NAME*" required />
     <FormulateInput type="text" label="LAST NAME*" required />
     <FormulateInput type="email" label="EMAIL ADDRESS*" required />
     <FormulateInput type="text" label="ORGANIZATION" required />
     <FormulateInput
+      class="col-span-2"
       type="select"
       label="EU RESIDENT*"
       placeholder="SELECT ONE"
