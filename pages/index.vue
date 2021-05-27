038cbb6403d020fa64cab3a475e26fbd8dae3414
Stephen Toms <s.mark.toms@gmail.com>
2021-05-27 02:24:56 -0400

    updated animation on form

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="flex min-h-screen">
-    <ContactForm class="mx-auto my-auto" />
+    <div class="mx-auto my-auto">
+      <div class="text-2xl font-bold text-center">CALL TO ACTION HEADER</div>
+      <div class="mb-5 w-72 text-center mx-auto">
+        Some descriptive text here to lead your viewers in explaining the
+        product.
+      </div>
+      <div class="flex">
+        <ContactForm class="mx-auto" />
+      </div>
+      <div class="text-xs mt-4">
+        Animation concept and UI inspired by creating functional UI elements and
+        truely responsive products.
+      </div>
+    </div>
   </div>
 </template>
